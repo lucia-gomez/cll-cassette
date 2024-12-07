2db840bc26492c1de8e7ff3f19b055ff52a9da27
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-12-07 15:36:56 -0500

    Update on PIN

--- remote_server/remote_server.ino
+++ remote_server/remote_server.ino
@@ -43,9 +43,9 @@ bool blueButtonState = HIGH; // Blue Button State
 const int switchLedPin = 13; // Switch LED Pin
 
 unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 20;
+const unsigned long debounceDelay = 30;
 
-const int potPin = 33;  // Potentiometer input pin
+const int potPin = 37;  // Potentiometer input pin
 
 unsigned long lastPotReadTime = 0;
 const unsigned long potReadInterval = 100;  // Read every 100ms
